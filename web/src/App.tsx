f114aabf33841bb901b3c0bed5e95e3624f2b8e4
Douglas Walas Rodrigues Pardim <douglas.skd@gmail.com>
2020-08-03 21:14:21 +0000

    Create project and add web part

---
+++ web/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+
+import logo from './assets/images/logo.svg'
+import logoDark from './assets/images/logo-dark.svg'
+
+const dark = true
+//{dark?logoDark:logo}
+
+function App() {
+  return (
+      <h1>Hello World</h1>
+  );
+}
+
+export default App;
